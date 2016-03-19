6a73b525bcaa9ec65e518eb9975fafc7433c2029
Shy Shalom <shooshx@gmail.com>
2016-03-19 21:53:14 +0200

    set one piece cube as the default, add google analytics

--- emscripten/one_piece_cube.js
+++ emscripten/one_piece_cube.js
@@ -96,45 +96,13 @@ var one_piece_cube_text = '<Generate definition>\n\
 \n\
 <Solutions>\n\
 :vector size = 6 \n\
-:count = 5 \n\
+:count = 1 \n\
 :s#0 = \n\
-;sc = 0 1 4 5 3 2 \n\
-;rt = 1 5 2 6 1 2 \n\
+;sc = 2 12 21 32 28 6 \n\
+;rt = 0 1 2 0 1 6 \n\
 ;symm = 0 \n\
-;numpic = 6 \n\
-;grp = 0 1 2 3 4 5 \n\
-;pic = 2 0 0 3 4 2 \n\
-\n\
-:s#1 = \n\
-;sc = 0 1 2 5 3 4 \n\
-;rt = 1 2 2 1 1 2 \n\
-;symm = 0 \n\
-;numpic = 6 \n\
-;grp = 0 1 2 3 4 5 \n\
-;pic = 2 0 0 3 4 2 \n\
-\n\
-:s#2 = \n\
-;sc = 3 5 2 1 0 4 \n\
-;rt = 1 1 6 2 1 6 \n\
-;symm = 0 \n\
-;numpic = 6 \n\
-;grp = 0 1 2 3 4 5 \n\
-;pic = 2 0 0 3 4 2 \n\
-\n\
-:s#3 = \n\
-;sc = 3 5 4 1 0 2 \n\
-;rt = 1 6 6 5 1 6 \n\
-;symm = 0 \n\
-;numpic = 6 \n\
-;grp = 0 1 2 3 4 5 \n\
-;pic = 2 0 0 3 4 2 \n\
-\n\
-:s#4 = \n\
-;sc = 1 0 2 3 5 4 \n\
-;rt = 7 3 4 3 4 4 \n\
-;symm = 0 \n\
-;numpic = 6 \n\
-;grp = 0 1 2 3 4 5 \n\
-;pic = 2 0 0 3 4 2 \n\
+;numpic = 7 \n\
+;grp = 0 0 1 2 3 4 5 \n\
+;pic = 2 4 0 0 3 4 2 \n\
 \n\
 '
